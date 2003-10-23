c3a84fced9c2cca465badb1701d205ee4866dd62
Bernhard R. Link <brlink@debian.org>
2003-10-23 18:32:07 +0000

    Added reading of .changes-fields

--- checkin.c
+++ checkin.c
@@ -45,9 +45,141 @@
 
 extern int verbose;
 
+/* Things to do when including a .changes-file:
+ *  - Read in the chunk of the possible signed file.
+ *    (In later versions possibly checking the signature)
+ *  - Parse it, extracting:
+ *  	+ Distribution
+ * 	+ Source
+ * 	+ Architecture
+ * 	+ Binary
+ * 	+ Version
+ * 	+ ...
+ * 	+ Files
+ *  - Calculate what files are expectable...
+ *  - Compare supplied filed with files expected.
+ *  - (perhaps: write what was done and changes to some logfile)
+ *  - add supplied files to the pool and register them in files.db
+ *  - add the .dsc-files via checkindsc.c
+ *  - add the .deb-filed via checkindeb.c
+ *
+ */
+
+struct changes {
+	/* Things read by changes_read: */
+	char *distribution,*source,
+	     *version;
+	struct strlist architectures,
+		       binaries,
+		       files;
+	char *control;
+};
+
+static void changes_free(struct changes *changes) {
+	if( changes != NULL ) {
+		free(changes->distribution);
+		free(changes->source);
+		free(changes->version);
+	}
+	free(changes);
+}
+
+static retvalue check(const char *filename,struct changes *changes,const char *field,int force) {
+	retvalue r;
+
+	r = chunk_checkfield(changes->control,field);
+	if( r == RET_NOTHING ) {
+		fprintf(stderr,"In '%s': Missing '%s' field!\n",filename,field);
+		if( !force )
+			return RET_ERROR;
+	}
+	return r;
+}
+
+static retvalue changes_read(const char *filename,struct changes **changes,int force) {
+	retvalue r;
+	struct changes *c;
+
+#define E(err,param...) { \
+		if( r == RET_NOTHING ) { \
+			fprintf(stderr,"In '%s': " err "\n",filename , ## param ); \
+			r = RET_ERROR; \
+	  	} \
+		if( RET_WAS_ERROR(r) ) { \
+			changes_free(c); \
+			return r; \
+		} \
+	}
+		
+	c = calloc(1,sizeof(struct changes));
+	if( c == NULL )
+		return RET_ERROR_OOM;
+
+	r = check(filename,c,"Format",force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+	
+	r = check(filename,c,"Date",force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+
+	r = signature_readsignedchunk(filename,&c->control);
+	if( RET_WAS_ERROR(r) )
+		return r;
+
+	r = chunk_getname(c->control,"Source",&c->source,0);
+	E("Missing 'Source' field");
+
+	r = chunk_getwordlist(c->control,"Binary",&c->binaries);
+	E("Missing 'Binary' field");
+
+	r = chunk_getwordlist(c->control,"Architecture",&c->architectures);
+	E("Missing 'Architecture' field");
+
+	r = chunk_getvalue(c->control,"Version",&c->version);
+	E("Missing 'Version' field");
+
+	r = chunk_getvalue(c->control,"Distribution",&c->distribution);
+	E("Missing 'Distribution' field");
+
+	r = check(filename,c,"Urgency",force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+
+	r = check(filename,c,"Maintainer",force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+
+	r = check(filename,c,"Description",force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+
+	r = check(filename,c,"Changes",force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+
+	r = chunk_getextralinelist(c->control,"Files",&c->files);
+	E("Missing 'Files' field");
+
+	r = check(filename,c,"Format",force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+
+	*changes = c;
+	return RET_OK;
+#undef E
+}
+
 /* insert the given .changes into the mirror in the <distribution>
  * if forcecomponent, forcesection or forcepriority is NULL
  * get it from the files or try to guess it. */
 retvalue changes_add(const char *dbdir,DB *references,DB *filesdb,const char *mirrordir,const char *forcecomponent,const char *forcedsection,const char *forcepriority,struct distribution *distribution,const char *changesfilename,int force) {
-	// TODO: implement it
+	retvalue r;
+	struct changes *changes;
+
+	r = changes_read(changesfilename,&changes,force);
+	if( RET_WAS_ERROR(r) )
+		return r;
+	
+	// TODO: implement the rest
 }
