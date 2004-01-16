01847b5fef1db07ab579f1951cf1d3f2cf7c1d24
Bernhard R. Link <brlink@debian.org>
2004-01-16 09:09:48 +0000

    Added strtupel.? in preperation of things to come

---
+++ strtupel.h
@@ -0,0 +1,18 @@
+#ifndef __MIRRORER_STRTUPEL_H
+#define __MIRRORER_STRTUPEL_H
+
+#ifndef __MIRRORER_ERROR_H
+#include "error.h"
+#warning "What's hapening here?"
+#endif
+
+typedef char strtupel;
+
+const char *strtupel_get(const strtupel *tupel,int index);
+strtupel *strtupel_fromarrays(int count,const char **strings,const size_t *lengths);
+strtupel *strtupel_fromvalues(int count,...);
+
+size_t strtupel_len(const strtupel *tupel);
+
+
+#endif
