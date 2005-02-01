01cb65a79cab5a0168aa75955252a8868ef887c2
Bernhard R. Link <brlink@debian.org>
2005-02-01 13:20:13 +0000

    allows multiple options given to --ignore seperated by comas.

--- ignore.c
+++ ignore.c
@@ -16,8 +16,10 @@
  */
 #include <config.h>
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 
 #include "ignore.h"
 
@@ -32,9 +34,9 @@ void init_ignores(void) {
 	}
 }
 
-retvalue add_ignore(const char *given) {
+static retvalue add(const char *given,size_t len) {
 	int i;
-	static const char *ignores[] = {
+	static const char * const ignores[] = {
 #define IGN(what) #what ,
 	VALID_IGNORES
 #undef IGN
@@ -43,16 +45,45 @@ retvalue add_ignore(const char *given) {
 	//TODO: allow multiple values sperated by some sign here...
 
 	for( i = 0 ; i < IGN_COUNT ; i++) {
-		if( strcmp(given,ignores[i]) == 0 ) {
+		if( strncmp(given,ignores[i],len) == 0 && ignores[i][len] == '\0' ) {
 			ignore[i] = TRUE;
 			break;
 		}
 	}
 	if( i == IGN_COUNT ) {
-		if( IGNORING("Ignoring","To Ignore",ignore,"Unknown --ignore value: '%s'!\n",given))
+		char *str = strndup(given,len);
+		if( IGNORING("Ignoring","To Ignore",ignore,"Unknown --ignore value: '%s'!\n",str?str:given)) {
+			free(str);
 			return RET_NOTHING;
-		else
+		} else {
+			free(str);
 			return RET_ERROR;
+		}
 	} else
 		return RET_OK;
+}
+
+retvalue add_ignore(const char *given) {
+	const char *g,*p;
+	retvalue r;
+
+	assert( given != NULL);
+
+	g = given;
+
+	while( 1 ) {
+		p = g;
+		while( *p != '\0' && *p !=',' )
+			p++;
+		if( p == g ) {
+			fprintf(stderr,"Empty ignore option in --ignore='%s'!\n",given);
+			return RET_ERROR_MISSING;
+		}
+		r = add(g,p-g);
+		if( RET_WAS_ERROR(r) )
+			return r;
+		if( *p == '\0' )
+			return RET_OK;
+		g = p+1;
+	}
 }
