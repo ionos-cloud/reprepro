0a0620c88c7eb1c5d1b7f83314152cd8cf5f158b
Bernhard R. Link <brlink@debian.org>
2008-07-31 13:42:56 +0000

    fix double free error with --export=never

--- distribution.c
+++ distribution.c
@@ -791,7 +791,7 @@ retvalue distribution_exportlist(enum exportwhen when, struct distribution *dist
 	if( when == EXPORT_NEVER ) {
 		if( verbose > 10 )
 			fprintf(stderr, "Not exporting anything as --export=never specified\n");
-		return distribution_freelist(distributions);
+		return RET_NOTHING;
 	}
 
 	for( d=distributions; d != NULL; d = d->next ) {
