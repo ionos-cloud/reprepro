ionos-cloud/reprepro

commit e0e842ba6adf78a29f160a1e09baff4b4867680a

properly return no tables state

database.c
@@ -334,6 +334,7 @@ retvalue database_listsubtables(const char *filename, struct strlist *result) {
 			strlist_done(&ids);
 			return r;
 		}
+		ret = RET_OK;
 		CLEARDBT(key);
 		CLEARDBT(data);
 	}
@@ -358,7 +359,7 @@ retvalue database_listsubtables(const char *filename, struct strlist *result) {
 		return RET_DBERR(dbret);
 	} else {
 		strlist_move(result, &ids);
-		return RET_OK;
+		return ret;
 	}
 }
 

commit b52fa81b4093447ab91cfcb7dbba5a05298fa555

fix problem introduced by e0e842ba6adf78a29f160a1e09baff4b4867680a

database.c
@@ -894,16 +894,16 @@ retvalue database_create(struct distribution *alldistributions, bool fast, bool 
 			database_close();
 			return r;
 		}
-		if (RET_IS_OK(r)) {
-			r = warnidentifers(&identifiers,
-					alldistributions, readonly);
-			if (RET_WAS_ERROR(r)) {
-				strlist_done(&identifiers);
-				database_close();
-				return r;
-			}
+		if (r == RET_NOTHING)
+			strlist_init(&identifiers);
+		r = warnidentifers(&identifiers,
+				alldistributions, readonly);
+		if (RET_WAS_ERROR(r)) {
 			strlist_done(&identifiers);
+			database_close();
+			return r;
 		}
+		strlist_done(&identifiers);
 	}
 	if (!allowunused && !fast && trackingfileexists)  {
 		struct strlist codenames;