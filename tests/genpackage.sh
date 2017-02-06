d55bfe6707c237584c107d9dd8f0817948ea9655
Benjamin Drung <benjamin.drung@profitbricks.com>
2017-02-06 17:49:07 +0100

    [testsuite] Use the host architecture by default
    To be able to build packages for the tests, use the host architecture
    (to avoid requiring a cross-compiler).

--- tests/genpackage.sh
+++ tests/genpackage.sh
@@ -19,7 +19,7 @@ Maintainer: me <guess@who>
 Standards-Version: 0.0
 
 Package: $PACKAGE
-Architecture: abacus
+Architecture: ${ARCH:-$(dpkg-architecture -qDEB_HOST_ARCH)}
 Description: bla
  blub
 
