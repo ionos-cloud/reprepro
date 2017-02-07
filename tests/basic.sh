8148a9d3db23bf258530039fe3c0750b3f37c08b
Benjamin Drung <benjamin.drung@profitbricks.com>
2017-02-07 17:20:29 +0100

    [testsuite] Add multiversion test cases

--- tests/basic.sh
+++ tests/basic.sh
@@ -96,6 +96,45 @@ test_limit() {
 	assertEquals "buster|main|$ARCH: hello 2.9-2" "$($REPREPRO -b $REPO list buster)"
 }
 
+test_older_version() {
+	cat >> $REPO/conf/incoming <<EOF
+Name: buster-upload
+IncomingDir: ../testpkgs
+TempDir: tmp
+Allow: buster
+Permit: older_version
+EOF
+	echo "Limit: 3" >> $REPO/conf/distributions
+	(cd $PKGS && PACKAGE=hello SECTION=main DISTRI=buster VERSION=2.9 REVISION=-1 ../genpackage.sh)
+	(cd $PKGS && PACKAGE=hello SECTION=main DISTRI=buster VERSION=2.9 REVISION=-2 ../genpackage.sh)
+	call $REPREPRO $VERBOSE_ARGS -b $REPO processincoming buster-upload hello_2.9-2_${ARCH}.changes
+	assertEquals "hello | 2.9-2 | buster | $ARCH, source" "$($REPREPRO -b $REPO ls hello)"
+	call $REPREPRO $VERBOSE_ARGS -b $REPO processincoming buster-upload hello_2.9-1_${ARCH}.changes
+	assertEquals "\
+hello | 2.9-2 | buster | $ARCH, source
+hello | 2.9-1 | buster | $ARCH, source" "$($REPREPRO -b $REPO ls hello)"
+}
+
+test_too_old_version() {
+	# Allow only one version per package in the archive
+	# Test if uploading an older version will not replace the newer version
+	# in the archive.
+	cat >> $REPO/conf/incoming <<EOF
+Name: buster-upload
+IncomingDir: ../testpkgs
+TempDir: tmp
+Allow: buster
+Permit: older_version
+EOF
+	echo "Limit: 1" >> $REPO/conf/distributions
+	(cd $PKGS && PACKAGE=hello SECTION=main DISTRI=buster VERSION=2.9 REVISION=-1 ../genpackage.sh)
+	(cd $PKGS && PACKAGE=hello SECTION=main DISTRI=buster VERSION=2.9 REVISION=-2 ../genpackage.sh)
+	call $REPREPRO $VERBOSE_ARGS -b $REPO processincoming buster-upload hello_2.9-2_${ARCH}.changes
+	assertEquals "hello | 2.9-2 | buster | $ARCH, source" "$($REPREPRO -b $REPO ls hello)"
+	call $REPREPRO $VERBOSE_ARGS -b $REPO processincoming buster-upload hello_2.9-1_${ARCH}.changes
+	assertEquals "hello | 2.9-2 | buster | $ARCH, source" "$($REPREPRO -b $REPO ls hello)"
+}
+
 test_remove() {
 	(cd $PKGS && PACKAGE=sl SECTION=main DISTRI=buster EPOCH="" VERSION=3.03 REVISION=-1 ../genpackage.sh)
 	call $REPREPRO $VERBOSE_ARGS -b $REPO -C main include buster $PKGS/sl_3.03-1_${ARCH}.changes
