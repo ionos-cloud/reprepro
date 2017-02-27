2ae79d153b194cedffef920db960b7b956c9e914
Benjamin Drung <benjamin.drung@profitbricks.com>
2017-02-27 14:13:04 +0100

    [testsuite] Add test cases for move* command
    Add test cases for the move, movesrc, movematched, movefilter commands.
    
    Signed-off-by: Benjamin Drung <benjamin.drung@profitbricks.com>

--- tests/multiversion.sh
+++ tests/multiversion.sh
@@ -199,4 +199,41 @@ buster|main|${ARCH}: hello 2.9-10
 buster|main|${ARCH}: hello 2.9-2+deb8u1" "$($REPREPRO -b $REPO list buster)"
 }
 
+test_move_specific() {
+	four_hellos
+	add_repo bullseye
+	$REPREPRO -b $REPO export bullseye
+	call $REPREPRO $VERBOSE_ARGS -b $REPO move bullseye buster hello=2.9-2
+	assertEquals "\
+buster|main|amd64: hello 2.9-10
+buster|main|amd64: hello 2.9-2+deb8u1
+buster|main|amd64: hello 2.9-1" "$($REPREPRO -b $REPO list buster)"
+	assertEquals "bullseye|main|amd64: hello 2.9-2" "$($REPREPRO -b $REPO list bullseye)"
+}
+
+test_movesrc_specific() {
+	four_hellos
+	add_repo bullseye
+	$REPREPRO -b $REPO export bullseye
+	call $REPREPRO $VERBOSE_ARGS -b $REPO movesrc bullseye buster hello 2.9-2
+	assertEquals "\
+buster|main|amd64: hello 2.9-10
+buster|main|amd64: hello 2.9-2+deb8u1
+buster|main|amd64: hello 2.9-1" "$($REPREPRO -b $REPO list buster)"
+	assertEquals "bullseye|main|amd64: hello 2.9-2" "$($REPREPRO -b $REPO list bullseye)"
+}
+
+test_movefilter_specific() {
+	four_hellos
+	add_repo bullseye
+	$REPREPRO -b $REPO export bullseye
+	call $REPREPRO $VERBOSE_ARGS -b $REPO movefilter bullseye buster 'Package (= hello), $Version (>> 2.9-2)'
+	assertEquals "\
+buster|main|amd64: hello 2.9-2
+buster|main|amd64: hello 2.9-1" "$($REPREPRO -b $REPO list buster)"
+	assertEquals "\
+bullseye|main|amd64: hello 2.9-10
+bullseye|main|amd64: hello 2.9-2+deb8u1" "$($REPREPRO -b $REPO list bullseye)"
+}
+
 . shunit2
